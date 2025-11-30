41491bbf420fa1e9258f2ee0ec5e05e1e0643741
pullavaras <linneagodunov@protonmail.com>
2025-11-30 20:13:43 +0200

    -

---
+++ popups/some-js.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function showPopup(id) {
+    document.getElementById(id).style.display = 'flex';
+}
+
+function closePopup(id) {
+    document.getElementById(id).style.display = 'none';
+}
+
+// Function to show end game popup, message and scrore
+function showEndPopup(message, score) {
+    document.getElementById('endMessage').innerText = message;
+    document.getElementById('finalScore').innerText = 'Lopulliset pisteet: ' + score;
+    showPopup('popupEnd');
+}
